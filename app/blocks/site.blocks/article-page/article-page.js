d83f6c96964ec1145844ef40b216d89a5bcd8027
Andrei Korolev <andreikoroleffs@gmail.com>
2020-10-15 14:05:26 +0300

    Адаптированы блоки

--- app/blocks/site.blocks/article-page/article-page.js
+++ app/blocks/site.blocks/article-page/article-page.js
@@ -9,11 +9,25 @@
 var articlePageTiles = new Swiper('.article-page__tiles-slider', {
 	direction: 'horizontal',
 	speed: 1000,
-	slidesPerView: 5,
-	slidesPerGroup: 5,
+	// slidesPerView: 5,
+	// slidesPerGroup: 5,
 	slidesOffsetAfter: 10,
 	navigation: {
 		nextEl: '.slider-button-next',
 		prevEl: '.slider-button-prev',
+	},
+	breakpoints: {
+		1024: {
+			slidesPerView: 4,
+			slidesPerGroup: 4,
+		},
+		1280: {
+			slidesPerView: 5,
+			slidesPerGroup: 5,
+		},
+		1920: {
+			slidesPerView: 5,
+			slidesPerGroup: 5,
+		}
 	},
 });
