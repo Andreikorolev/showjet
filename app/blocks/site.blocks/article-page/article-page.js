410d6ed23e0e40be149186b6ddde8c72bbe68710
Andrei Korolev <andreikoroleffs@gmail.com>
2020-10-15 12:50:07 +0300

    Добавил блок карточек на страницу статьи. Адаптировал блоки карточек 2/3/4

--- app/blocks/site.blocks/article-page/article-page.js
+++ app/blocks/site.blocks/article-page/article-page.js
@@ -1,11 +1,11 @@
-$(window).scroll(function(){
-	var windowScrollTop = $(window).scrollTop();
-	if (windowScrollTop > 250 && windowScrollTop < ($('.article').height() + $(".article").offset().top) - $(".header").height() - $(".article__soc-buttons").height()) {
-		$(".article__soc-buttons").addClass('fixed');
-	} else {
-		$(".article__soc-buttons").removeClass('fixed');
-	}
-});
+// $(window).scroll(function(){
+// 	var windowScrollTop = $(window).scrollTop();
+// 	if (windowScrollTop > 250 && windowScrollTop < ($('.article').height() + $(".article").offset().top) - $(".header").height() - $(".article__soc-buttons").height()) {
+// 		$(".article__soc-buttons").addClass('fixed');
+// 	} else {
+// 		$(".article__soc-buttons").removeClass('fixed');
+// 	}
+// });
 var articlePageTiles = new Swiper('.article-page__tiles-slider', {
 	direction: 'horizontal',
 	speed: 1000,
