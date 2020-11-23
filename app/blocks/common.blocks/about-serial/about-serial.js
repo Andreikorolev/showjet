84d58f81925c0abe726898edf7ba861a7056ab5e
Andrei Korolev <andreikoroleffs@gmail.com>
2020-11-24 01:18:58 +0300

    Правки по замечаниям заказчикам

--- app/blocks/common.blocks/about-serial/about-serial.js
+++ app/blocks/common.blocks/about-serial/about-serial.js
@@ -10,13 +10,31 @@ $(".about-serial__navigation-item").click(function(){
 });
 
 $(".actors-creators__item").on('click', function(){
-	console.log('click');
 	$(".actors-creators__person").addClass("active");
 	$(".actors-creators__wrap").hide();
 });
 
 $(".actors-creators__person-back-button").on('click', function(){
-	console.log('click');
 	$(".actors-creators__person").removeClass("active");
 	$(".actors-creators__wrap").show();
+});
+
+function scaleAboutSerialInfoWrap() {
+	var body = document.querySelector('body');
+	var infoWrapHeight = $('.about-serial__info-wrap').height();
+	var trailerHeight = $('.about-serial__trailer').height();
+	body.style.setProperty('--about-serial-left-wrap-scale', (trailerHeight / infoWrapHeight));
+}
+
+$(window).on('load', function() {
+	var windowWidth = window.innerWidth;
+	if (windowWidth >= 2500) {
+		scaleAboutSerialInfoWrap();
+	}
+});
+$(window).resize(function() {
+	var windowWidth = window.innerWidth;
+	if (windowWidth >= 2500) {
+		scaleAboutSerialInfoWrap();
+	}
 });
